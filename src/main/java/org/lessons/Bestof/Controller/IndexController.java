64ff995977ecf2f1a93c1ce46722550a3072c883
Giacomotestori <giacomo.testori10@gmail.com>
2023-09-12 15:27:55 +0200

    STEP 1 completed- html homepage completed

--- src/main/java/org/lessons/Bestof/Controller/IndexController.java
+++ src/main/java/org/lessons/Bestof/Controller/IndexController.java
@@ -1,7 +1,7 @@
 package org.lessons.Bestof.Controller;
 
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +14,7 @@ public class IndexController {
     @GetMapping
     public String index(Model model){
         String myName = "Giacomo Testori";
-        model.addText("name");
+        model.addAttribute("name", myName);
         return "homepage";
     }
 
